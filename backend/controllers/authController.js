85600ecc731a82bec87a23a7bb8f87e24d235ca6
Joana Marohn <j.marohn.dev@outlook.de>
2025-06-04 12:39:10 +0200

    added api/users/login route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,7 +10,7 @@ const register = async (req, res) => {
     if (!email || !password) {
       return res
         .status(400)
-        .json({ message: "E-Mail und Passwort sind erforderlich" });
+        .json({ message: "E-Mail und Passwort sind erforderlich!" });
     }
 
     if (existingUser) {
@@ -33,4 +33,40 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "E-Mail und Passwort sind erforderlich!" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "E-Mail oder Passwort ungültig!" });
+    }
+
+    const doesMatch = await user.comparePassword(password);
+    if (!doesMatch) {
+      return res
+        .status(400)
+        .json({ message: "Ungültiges Passwort, bitte erneut prüfen!" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.status(200).json({ message: "Login erfolgreich", token });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+};
+
+module.exports = { register, login };
