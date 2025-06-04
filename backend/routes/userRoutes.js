e441b263cee8420144842ccccd8bdea5bfff67e4
Joana Marohn <j.marohn.dev@outlook.de>
2025-06-04 13:41:05 +0200

    Implemented user authentication with JWT and verifyToken middleware Added protected test route /api/users/secret

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/authController");
+const verifyToken = require("../middleware/authMiddleware");
 
 router.post("/register", register);
 router.post("/login", login);
+
+router.get("/secret", verifyToken, (req, res) => {
+  res.json({ message: "Du hast Zugriff!", userId: req.user });
+});
 
 module.exports = router;
