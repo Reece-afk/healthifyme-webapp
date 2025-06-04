d39f5d5bfa219e430e9458b661d04dd877b32ccf
Joana Marohn <j.marohn.dev@outlook.de>
2025-06-04 12:15:40 +0200

    Added /api/users/register route and test registration successfully

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,11 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const userRoutes = require("./routes/userRoutes");
 
 app.use(express.json());
 app.use(cors());
+app.use("/api/users", userRoutes);
 
 connectDB();
 
