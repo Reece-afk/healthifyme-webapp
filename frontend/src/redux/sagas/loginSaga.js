8250291a395f160662f53ce26836a00f01e75118
Joana Marohn <j.marohn.dev@outlook.de>
2025-06-05 17:59:47 +0200

    implemented auth token handling with js-cookie and axios interceptor

--- frontend/src/redux/sagas/loginSaga.js
+++ frontend/src/redux/sagas/loginSaga.js
@@ -1,10 +1,12 @@
 import { call, put, takeLatest } from "redux-saga/effects";
 import api from "../../services/api";
 import { loginSuccess, loginFailure, loginStart } from "../authSlice";
+import Cookies from "js-cookie";
 
 function* loginSaga(action) {
   try {
     const response = yield call(api.post, "/users/login", action.payload);
+    Cookies.set("token", response.data.token, { expires: 1 });
     yield put(loginSuccess(response.data));
   } catch (error) {
     yield put(
--- frontend/src/redux/sagas/registerSaga.js
+++ frontend/src/redux/sagas/registerSaga.js
@@ -1,10 +1,12 @@
 import { call, put, takeLatest } from "redux-saga/effects";
 import api from "../../services/api";
 import { loginSuccess, loginFailure, registerStart } from "../authSlice";
+import Cookie from "js-cookie";
 
 function* registerSaga(action) {
   try {
     const response = yield call(api.post, "/users/register", action.payload);
+    Cookie.set("token", response.data.token, { expires: 1 });
     yield put(loginSuccess(response.data));
   } catch (error) {
     yield put(
