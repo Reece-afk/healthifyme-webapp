4580bb21c354c392864cf06f3ad138bbecf03cf5
Joana Marohn <j.marohn.dev@outlook.de>
2025-06-05 16:22:51 +0200

    corrected typo in Register.jsx, added registerSaga to rootSaga

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 function Register() {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useSta("");
+  const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
 
   const dispatch = useDispatch();
--- frontend/src/redux/sagas/index.js
+++ frontend/src/redux/sagas/index.js
@@ -1,6 +1,7 @@
 import { all } from "redux-saga/effects";
 import watchLoginSaga from "./loginSaga";
+import watchRegisterSaga from "./registerSaga";
 
 export default function* rootSaga() {
-  yield all([watchLoginSaga()]);
+  yield all([watchLoginSaga(), watchRegisterSaga()]);
 }
